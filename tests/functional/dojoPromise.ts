09dee3d9aac657a58e34916747c781a905cbd033
Rory Mulligan <rorticus@gmail.com>
2016-12-15 09:22:23 -0600

    Better example of async/await

--- tests/functional/dojoPromise.ts
+++ tests/functional/dojoPromise.ts
@@ -1,8 +1,13 @@
 import DPromise from '../../src/Promise';
 
 export default async function main(callback: (n: number) => void) {
+	function reallyGetNumber() {
+		return DPromise.resolve(21);
+	}
+
 	async function getNumber() {
-		return DPromise.resolve(42);
+		const num = await reallyGetNumber();
+		return num * 2;
 	}
 
 	const result = await getNumber();
--- tests/functional/nativePromise.ts
+++ tests/functional/nativePromise.ts
@@ -1,6 +1,11 @@
 export default async function main(callback: (n: number) => void) {
+	function reallyGetNumber() {
+		return Promise.resolve(21);
+	}
+
 	async function getNumber() {
-		return Promise.resolve(42);
+		const num = await reallyGetNumber();
+		return num * 2;
 	}
 
 	const result = await getNumber();
